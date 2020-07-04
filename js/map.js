605be5bec595204c3f8889eb21618b9af238168d
dinawatcher <din4ess@gmail.com>
2020-07-04 21:26:38 +0300

    получает данные с сервера для создания пинов

--- js/map.js
+++ js/map.js
@@ -1,13 +1,11 @@
 'use strict';
 
 (function () {
-  var ADS_COUNT = 8;
   var PIN_MAIN_SIZE = 65;
   var PIN_MAIN_POINTER = 22;
 
   var hotelMap = document.querySelector('.map');
   var mapPinMain = document.querySelector('.map__pin--main');
-  var pins = window.offer.createAds(ADS_COUNT);
   var pinMainLocationX = Math.round(parseInt(mapPinMain.style.left, 10) + PIN_MAIN_SIZE / 2);
   var pinMainLocationY = Math.round(parseInt(mapPinMain.style.top, 10) + PIN_MAIN_SIZE / 2);
   var pinMainLocationYActive = Math.round(parseInt(mapPinMain.style.top, 10)) + PIN_MAIN_SIZE + PIN_MAIN_POINTER;
@@ -25,9 +23,25 @@
     window.form.address.value = pinMainLocationX + ',' + ' ' + pinMainLocationY;
   };
 
+  var getData = function () {
+    var urlLoad = 'https://javascript.pages.academy/keksobooking/data';
+
+    var onSuccessLoad = function (data) {
+      for (var i = 0; i < data.length; i++) {
+        window.offer.getAds(data[i]);
+      }
+      window.pin.renderPins(data);
+      activatePage();
+    };
+    var onErrorLoad = function () {
+      disabledOnLoad();
+    };
+
+    window.backend.load(urlLoad, onSuccessLoad, onErrorLoad);
+  };
+
   var activatePage = function () {
     hotelMap.classList.remove('map--faded');
-    window.pin.renderPins(pins);
     window.form.activateInputs(window.form.formFieldsets);
     window.form.activateInputs(window.form.adFormSelects);
     window.form.activateInputs(window.form.filterFormSelects);
@@ -39,17 +53,14 @@
 
   var onPinMainAction = function (evt) {
     if (evt.button === 0 || evt.code === 'Enter') {
-      activatePage();
+      getData();
     }
   };
-
-  disabledOnLoad();
 
   mapPinMain.addEventListener('mousedown', onPinMainAction);
   mapPinMain.addEventListener('keydown', onPinMainAction);
 
   window.map = {
     hotelMap: hotelMap,
-    pins: pins,
   };
 })();
--- js/offer.js
+++ js/offer.js
@@ -1,64 +1,39 @@
 'use strict';
 
 (function () {
-  var MAX_X = 1200;
-  var MIN_Y = 130;
-  var MAX_Y = 630;
-  var MIN_PRICE = 10000;
-  var MAX_PRICE = 50000;
-  var MIN_ROOMS = 1;
-  var MAX_ROOMS = 3;
-  var MIN_GUESTS = 0;
-  var MAX_GUESTS = 2;
+  // var MAX_X = 1200;
+  // var MIN_Y = 130;
+  // var MAX_Y = 630;
+  // var MIN_PRICE = 10000;
+  // var MAX_PRICE = 50000;
+  // var MIN_ROOMS = 1;
+  // var MAX_ROOMS = 3;
+  // var MIN_GUESTS = 0;
+  // var MAX_GUESTS = 2;
   var HOTEL_TYPES = {
     palace: 'Дворец',
     flat: 'Квартира',
     house: 'Дом',
     bungalo: 'Бунгало',
   };
-  var CHECK_TIME = ['12:00', '13:00', '14:00'];
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var HOTEL_PHOTOS = [
-    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-  ];
+  // var CHECK_TIME = ['12:00', '13:00', '14:00'];
+  // var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  // var HOTEL_PHOTOS = [
+  //   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  //   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  //   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  // ];
+
   var ads = [];
 
-  var createAds = function (num) {
-
-    for (var i = 0; i < num; i++) {
-      var locationX = window.randomize.getRandomNumber(0, MAX_X);
-      var locationY = window.randomize.getRandomNumber(MIN_Y, MAX_Y);
-
-      ads[i] = {
-        'author': {
-          'avatar': 'img/avatars/user0' + (i + 1) + '.png',
-        },
-        'offer': {
-          'title': 'Заголовок объявления',
-          'address': locationX + ', ' + locationY,
-          'price': window.randomize.getRandomNumber(MIN_PRICE, MAX_PRICE),
-          'type': window.randomize.getTypes(HOTEL_TYPES),
-          'rooms': window.randomize.getRandomNumber(MIN_ROOMS, MAX_ROOMS),
-          'guests': window.randomize.getRandomNumber(MIN_GUESTS, MAX_GUESTS),
-          'checkin': CHECK_TIME[window.randomize.getRandomNumber(0, CHECK_TIME.length - 1)],
-          'checkout': CHECK_TIME[window.randomize.getRandomNumber(0, CHECK_TIME.length - 1)],
-          'features': window.randomize.getFeatures(FEATURES),
-          'description': 'Описание (не обязательно)',
-          'photos': window.randomize.getRandomArray(HOTEL_PHOTOS),
-        },
-        'location': {
-          x: locationX,
-          y: locationY,
-        }
-      };
-    }
-    return ads;
+  var getAds = function (data) {
+    ads.push(data);
   };
+
+
   window.offer = {
-    createAds: createAds,
     ads: ads,
+    getAds: getAds,
     hotels: HOTEL_TYPES,
   };
 })();
